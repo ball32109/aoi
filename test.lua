f8ab756dff1a7d16eb71865ec5d2d2ca9c10bbbd
mrq <362651591@qq.com>
2014-10-26 01:32:28 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -7,10 +7,32 @@ for i = 1 ,100 do
   aoi_inst:enter(i,{x = math.random(1,110),y = math.random(1,110)})
 end
 
-aoi_inst:enter(0,{x = 50,y = 50})
+function dump(t)
+  for k,v in pairs(t) do
+    print(v)
+  end
+end
 
-aoi_inst:update(0,{x = 55,y = 55})
-aoi_inst:update(0,{x = 55,y = 55})
-aoi_inst:update(0,{x = 55,y = 55})
-aoi_inst:update(0,{x = 55,y = 55})
-aoi_inst:update(0,{x = 55,y = 55})aoi_inst:update(0,{x = 55,y = 55})
+local enter_self,enter_other = aoi_inst:enter(0,{x = 50,y = 50})
+print("enter_self")
+dump(enter_self)
+print("enter_other")
+dump(enter_other)
+
+function move(id,pos)
+  local enter_self,enter_other,leave_self,leave_other = aoi_inst:update(0,{x = pos.x,y = pos.y})
+  print("enter_self")
+  dump(enter_self)
+  print("enter_other")
+  dump(enter_other)
+  print("leave_self")
+  dump(leave_self)
+  print("leave_other")
+  dump(leave_other)
+end
+
+move(0,{x = 59,y = 59})
+move(0,{x = 65,y = 65})
+move(0,{x = 75,y = 75})
+move(0,{x = 85,y = 85})
+move(0,{x = 15,y = 55})
